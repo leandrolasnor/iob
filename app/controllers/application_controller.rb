4a26803752d4b3115b1741576b53be036f2429ff
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-12-15 03:28:29 +0000

    chore: controller base class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
-class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+# frozen_string_literal: true
+
+class ApplicationController < ApiController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    render head: :not_found
+  end
 end
