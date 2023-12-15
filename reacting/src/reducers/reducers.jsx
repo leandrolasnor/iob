7428b054e497902e97292db60e8fcf861441995e
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-12-15 23:14:48 +0000

    chore: add frontend in project

---
+++ reacting/src/reducers/reducers.jsx
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import {reducer as toastr} from 'react-redux-toastr';
+
+const rootReducer = combineReducers({
+  toastr
+});
+
+export default rootReducer;
